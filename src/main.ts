0246ebd6b357114aa0fbe5524cbd51d86508ba87
Aleksandr <zybkob@f-case.ru>
2022-11-06 10:43:28 +0300

    swagger cors 13

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
   await app.listen(3000);
 }
