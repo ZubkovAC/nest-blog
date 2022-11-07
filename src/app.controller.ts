2634b1259928a50706a16a6212fc8f8a54b5d25c
Aleksandr <zybkob@f-case.ru>
2022-11-07 11:46:57 +0300

    test swagger 1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,34 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import {
-  ApiResponse,
-  ApiTags,
-  DocumentBuilder,
-  SwaggerModule,
-} from '@nestjs/swagger';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('test-server')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Hello World!!22!',
-  // })
-  // @ApiResponse({ status: 500, description: 'crash - server' })
-  async getHello() {
-    // return this.appService.getHello();
-    const app = await NestFactory.create(AppModule);
-    const config = await new DocumentBuilder()
-      .setTitle('"Study, study and study again." @ Lenin')
-      .setDescription('educational API description')
-      .setVersion('1.0')
-      .addTag('cats')
-      .build();
-    const document = await SwaggerModule.createDocument(app, config);
-    return SwaggerModule.setup('api', app, document);
+  @ApiResponse({
+    status: 200,
+    description: 'Hello World!!22!',
+  })
+  @ApiResponse({ status: 500, description: 'crash - server' })
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
