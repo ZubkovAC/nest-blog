51133a4513a460c6f381a9f6e11b173a806e1eef
Aleksandr <zybkob@f-case.ru>
2022-11-10 08:34:08 +0300

    blog 4  ( fix searchTerm )

--- src/bloggers/bloggers.repository.ts
+++ src/bloggers/bloggers.repository.ts
@@ -35,11 +35,12 @@ export class BloggerRepository {
     sortDirection: any,
   ) {
     const skipCount = (pageNumber - 1) * pageSize;
-    const query = { name: { $regex: /[searchNameTerm]/ } };
-    const totalCount = await this.blogRepository.countDocuments(query);
+    const totalCount = await this.blogRepository.countDocuments({
+      name: { $regex: searchNameTerm },
+    });
+
     const bloggersRestrict = await this.blogRepository
-      // .find({ query }, '-_id -__v')
-      .find({ name: { $regex: /[searchNameTerm]/ } }, '-_id -__v')
+      .find({ name: { $regex: searchNameTerm } }, '-_id -__v')
       .sort({ [sort]: sortDirection })
       .skip(skipCount)
       .limit(pageSize)
