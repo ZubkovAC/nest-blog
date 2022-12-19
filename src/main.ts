97206661b6df0fa82461dbbe1580bf0c332e002e
Aleksandr <zybkob@f-case.ru>
2022-12-19 23:18:42 +0300

    v12

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors({ credentials: true, origin: true, allowedHeaders: '*' });
+  app.enableCors({ credentials: true, origin: true });
+  // app.enableCors({ allowedHeaders: '*', origin: '*' });
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
