5447c144a8cb2b54bf89ce13cccd0b0646b9c3a9
Aleksandr <zybkob@f-case.ru>
2022-12-20 12:56:15 +0300

    v12 refreshToken - body ( httpOnly - false , secure - true )

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -288,7 +288,7 @@ export class AuthController {
     console.log('resLogin.passwordRefresh', resLogin.passwordRefresh);
     response.cookie('refreshToken', resLogin.passwordRefresh, {
       // expires: new Date(Date.now() + 3600 * 1000 * 24 * 180 * 1),
-      sameSite: 'strict',
+      sameSite: 'none',
       // httpOnly: true,
       secure: true,
     });
